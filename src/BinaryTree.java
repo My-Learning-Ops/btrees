37b6b0921952d8fc9b0395a1b5c451b055cc1ce3
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-06-03 08:42:49 -0400

    Setup Node Class

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -3,8 +3,30 @@ import java.util.Comparator;
 
 public class BinaryTree<T> {
 
-  class Node<T> {
+  // Node class representing each node in the binary tree
+  static class Node<T> {
+    T value;
+    Node<T> leftTree;
+    Node<T> rightTree;
+
+    // Constructor creating a node with a value
+    Node(T val) {
+      value = val;
+      leftTree = null;
+      rightTree = null;
+    }
+
+    // Constructor creating a node with a value and left/right children
+    Node(T val, Node<T> left, Node<T> right) {
+      value = val;
+      leftTree = left;
+      rightTree = right;
+    }
   }
+
+  private Node<T> root;
+  private Comparator<T> comparator;
+  private int size;
 
   
 }
